60e6d6bb54d648ae308c72e747b96bff8b2fc60e
ConstMoissanite <3051376580@qq.com>
2025-03-23 14:20:15 +0800

    refactor&chore:补入go.mod,重构存储结构在新文件中

---
+++ go.mod
@@ -0,0 +1,12 @@
+module go-bitcoin
+
+go 1.24
+
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/btcsuite/btcutil v1.0.2
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
+	github.com/holiman/uint256
+)
+
+require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
---
+++ personal def.go
@@ -0,0 +1,50 @@
+package main
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+	"time"
+)
+//这里是我觉得好一些的原始定义
+type Block_Content struct
+{
+	Version uint64
+	TimeStamp uint64
+	Bits uint64//这里其实32位就足够了
+	Nonce uint64
+	
+	PrevHash []byte
+	MerkleRoot []byte
+	
+	Txs []*Transaction
+}
+
+type Block struct
+{
+	Content Block_Content
+	PresHash []byte
+}
+
+type BlockChain_Node struct
+{
+	Block
+	PrevPtr *Block
+}//链表式实现，适用于物理存储
+
+type BlockChain struct
+{
+	length uint64
+	tail *BlockChain_Node
+}
+
+/*
+
+func rewardcalculator(bc *BlockChain)(res float64)
+{
+ const term uint64=210000
+ res=math.Pow(2,bc.length/term)*50
+ return
+}
+
+
